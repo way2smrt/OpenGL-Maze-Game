8cb3d30cc9d65d0c7b67c6256f95a8a7646114ca
unknown <S338867567@BELL-BA7-120585.ad.ocdsb.ca>
2015-06-01 13:47:48 -0400

    Working on object loading. Removed obselete or redundant classes.

--- src/com/battleslug/flare/object/Face.java
+++ src/com/battleslug/flare/object/Face.java
@@ -1,13 +1,33 @@
 package com.battleslug.flare.object;
 
+import com.battleslug.glbase.geometry.*;
+
 public class Face {
 	private Material mat;
+	private Point[] point;
 	
-	public Face(Material mat){
+	public Face(Material mat, Point[] point){
 		this.mat = mat;
 	}
 	
 	public Material getMaterial(){
 		return mat;
+	}
+	
+	public int getNumPoints(){
+		return point.length;
+	}
+	
+	public Point getPoint(int point){
+		if(point >= this.point.length){
+			return this.point[point];
+		}
+		else {
+			return null;
+		}
+	}
+	
+	public void addPoint(){
+		
 	}
 }
--- src/com/battleslug/flare/object/Material.java
+++ src/com/battleslug/flare/object/Material.java
@@ -4,9 +4,15 @@ import com.battleslug.glbase.*;
 
 public class Material {
 	private Texture tex;
+	private String ID;
 	
-	public Material(Texture tex){
+	public Material(String ID, Texture tex){
 		this.tex = tex;
+		this.ID = ID;
+	}
+	
+	public String getID(){
+		return ID;
 	}
 	
 	public Texture getTexture(){
