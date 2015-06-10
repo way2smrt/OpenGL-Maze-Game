d3269f850e4d6faa5ebc3561b508350bded54107
way2smrt <way2smrt@gmail.com>
2015-06-10 07:43:36 -0400

    Working on sound

--- src/com/battleslug/glbase/Sound.java
+++ src/com/battleslug/glbase/Sound.java
@@ -2,53 +2,74 @@ package com.battleslug.glbase;
 
 import java.io.FileNotFoundException;
 
-import static org.lwjgl.openal.AL10.*;
-
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.ByteBuffer;
 import java.io.FileInputStream;
+import javax.sound.sampled.*;
+import java.io.File;
+
+import org.lwjgl.BufferUtils;
+import static org.lwjgl.openal.AL10.*;
+import static org.lwjgl.openal.AL.*;
+import static org.lwjgl.openal.Util.*;
+
+import org.lwjgl.util.WaveData;
 
 public class Sound {
-	private WaveData waveData;
-	
-	private int buffer;
-	private int source;
+	private IntBuffer buffer;
+	private IntBuffer source;
+
+	//sound location and velocity data
+	FloatBuffer sourcePos = (FloatBuffer)BufferUtils.createFloatBuffer(3).put(new float[] { 0.0f, 0.0f, 0.0f }).rewind();
+	FloatBuffer sourceVel = (FloatBuffer)BufferUtils.createFloatBuffer(3).put(new float[] { 0.0f, 0.0f, 0.0f }).rewind();
+	FloatBuffer listenerPos = (FloatBuffer)BufferUtils.createFloatBuffer(3).put(new float[] { 0.0f, 0.0f, 0.0f }).rewind();
+	FloatBuffer listenerVel = (FloatBuffer)BufferUtils.createFloatBuffer(3).put(new float[] { 0.0f, 0.0f, 0.0f }).rewind();
+	FloatBuffer listenerOri = (FloatBuffer)BufferUtils.createFloatBuffer(6).put(new float[] { 0.0f, 0.0f, -1.0f,  0.0f, 1.0f, 0.0f }).rewind();
 	
 	public Sound(String file){
 		loadALData(file);
 	}
 	
-	public void loadALData(String file) {
-		//generate buffers and sources
-		int buffer = alGenBuffers();
-		int source = alGenSources();
+	private void loadALData(String file){
+		//denullify our buffers
+		IntBuffer buffer = BufferUtils.createIntBuffer(1);
+		IntBuffer source = BufferUtils.createIntBuffer(1);
 		
-		//load wave data
-		try {
-			waveData =  WaveData.create(new FileInputStream(file));
-			
-		}
-		catch(FileNotFoundException e){
-			System.out.println("Could not find file: "+file);
-			e.printStackTrace();
-		}
+		int[] format = new int[1];
+		int[] size = new int[1];
+		ByteBuffer[] data = new ByteBuffer[1];
+		int[] freq = new int[1];
+		int[] loop = new int[1];
+
+		//load wave data into buffers
+		WaveData waveFile = WaveData.create(AudioSystem.getAudioInputStream(new File(file)));
+		//alutLoadWAVFile(file, format, data, size, freq, loop);
 		
-		try {
-			//copy to buffer
-			alBufferData(buffer, waveData.format, waveData.data, waveData.samplerate);
-		} 
-		finally {
-			if(waveData != null){
-				waveData.dispose();
-			}
-			else {
-				System.out.println("Error: WaveData is null");
-			}
-		}
+		//buffer the data into openal
+		alBufferData(buffer.get(0), format[0], data[0], size[0], freq[0]);
+
+		waveFile.dispose();
+		
+		//generate the sources from buffer
+		alGenSources(source);
+		
+		//generate the buffers
+		alGenBuffers(buffer);
 			
 		//set up source input
-		alSourcei(source, AL_BUFFER, buffer);
+		alSourcei(source.get(0), AL_BUFFER, buffer.get(0));
 		
-		//loop the sound
-		alSourcei(source, AL_LOOPING, AL_TRUE);
+		//set up looping
+		alSourcei(source.get(0), AL_LOOPING, AL_TRUE);
+		
+		setListenerValues();
+	}
+	
+	private void setListenerValues(){
+		alListener(AL_POSITION, listenerPos);
+		alListener(AL_VELOCITY, listenerVel);
+		alListener(AL_ORIENTATION, listenerOri);
 	}
 	
 	public void destroy(){ 
